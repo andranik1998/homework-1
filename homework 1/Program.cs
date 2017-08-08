26d5172e4cf28ccc09f7f6e1ab3d0117d847d6a2
andranik1998 <ando.paylozyan@mail.ru>
2017-08-08 18:18:59 +0400

    Add project files.

---
+++ homework 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            do
+            {
+                Console.WriteLine("gri Andoi amenasirac tiv@");
+
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            while (number != 7);
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
